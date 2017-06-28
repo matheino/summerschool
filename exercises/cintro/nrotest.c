77526da764f04a71b069946d20fd8baebcf348b1
matheino <matias.heino@aalto.fi>
2017-06-28 20:16:31 +0300

    cintro

---
+++ exercises/cintro/nrotest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  int a = -1;
+
+  if (a < 0){
+  printf("a is negative\n");
+
+  } else if (a == 0) {
+  printf("a is zero\n");
+
+  } else {
+  printf("a is positive\n");
+  }
+}
